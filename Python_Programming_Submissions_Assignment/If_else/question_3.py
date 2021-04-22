8dda34d1db02282eb8bf0fb22f974dd9694bab6c
Devishakti2230 <devishakti2000@gmail.com>
2021-04-23 00:10:49 +0530

    Update question_3.py

--- Python_Programming_Submissions_Assignment/If_else/question_3.py
+++ Python_Programming_Submissions_Assignment/If_else/question_3.py
@@ -1,1 +1,13 @@
 
+# Python code that asks a user how many pizza slices they want.The pizzeria charges Rs 123.00 a slice. if user order even number of slices, 
+# price per slice is Rs 120.00. Print the total price depending on how many slices user orders.
+
+a = int(input())
+total=0
+if(a % 2==0):
+    b=120*a
+    total+=b
+else:
+    c=130*a
+    total+=c
+print(total)
