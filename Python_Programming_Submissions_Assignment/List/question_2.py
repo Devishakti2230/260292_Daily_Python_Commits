ce9253d444fcd51828962968adfc1e5886d711c6
Devishakti2230 <devishakti2000@gmail.com>
2021-04-22 23:54:55 +0530

    Update question_2.py

--- Python_Programming_Submissions_Assignment/List/question_2.py
+++ Python_Programming_Submissions_Assignment/List/question_2.py
@@ -2,8 +2,8 @@
 # Change the position of every n-th value with the (n+1)th in a list
 
 from itertools import zip_longest, chain, tee
-def replace2copy(lst):
-    lst1, lst2 = tee(iter(lst), 2)
-    return list(chain.from_iterable(zip_longest(lst[1::2], lst[::2])))
-n = [0,1,2,3,4,5]
-print(replace2copy(n))
+def replace2copy(list_1):
+    list_2, list_3 = tee(iter(list_1), 2)
+    return list(chain.from_iterable(zip_longest(list_1[1::2], list_1[::2])))
+list_4 = [0,1,2,3,4,5]
+print(replace2copy(list_4))
