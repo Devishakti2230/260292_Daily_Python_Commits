53190100819fd9e8fd535fa8f76941e92635f482
Devishakti2230 <devishakti2000@gmail.com>
2021-04-22 23:20:19 +0530

    Update question2.py

---
+++ Python_Programming_Submissions_Assignment/If_else/question_2.py
@@ -0,0 +1,18 @@
+
+#Write a python program to check the user input abbreviation.
+#If the user enters "lol", print "laughing out loud".
+#If the user enters "rofl", print "rolling on the floor laughing".
+#If the user enters "lmk", print "let me know".
+#If the user enters "smh", print "shaking my head".
+
+inp=input()
+if(inp=="lol"):
+    print("laughing out loud")
+elif(inp=="rofl"):
+    print("rolling on the floor laughing")
+elif(inp=="lmk"):
+    print("let me know")
+elif(inp=="smh"):
+    print("shaking my head")
+else:
+    print("Plz enter lol or rofl or lmk or smh")
